6f5a71dd84398cd3b6857c39a44deacf87f779d6
fredboys <freddieboys@hotmail.co.uk>
2023-02-02 14:08:37 +0000

    React setstate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallBack from './components/StatefulGreetingWithCallBack';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
+      <StatefulGreeting greeting="Im a stateful class component" name="Freddie"/>
+      <StatefulGreetingWithCallBack greeting="Im a stateful class component" name="Freddie"/>
+      <StatefulGreetingWithPrevState greeting="Im a stateful class component" name="Freddie"/>
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const FunctionalGreetingWithProps = (props) => {
     console.log(props);
-    return <h1>Hello, {props.name}! I see you're {props.age} years old.
-    gt {props.greeting}</h1>
+    return <h1>Hello, {props.name}! I see you're {props.age} years old. {props.greeting}</h1>
 }
 export default FunctionalGreetingWithProps;
